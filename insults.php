27784962895d8b703f5733eb2d0cc34e4745cdc7
J.M. Soorya Narayan <SooryaN@users.noreply.github.com>
2017-09-03 13:57:17 +0530

    Update insults.php

--- insults.php
+++ insults.php
@@ -15,6 +15,16 @@ $insults = [
     "Lol bye",
     "You should become a random string generator",
     "Del-Tata ByeBye",
+    "git blind --hard",
+    "Mind that the 'g' in Delta is silent",
+    "See you in shell",
+    "Insert coin to try again",
+    "REKT",
+    "Try this - :(){ :|:& };:",
+    "It's not you, it's me",
+    "/dev/brain: No such file or directory",
+    "Should contain an uppercase letter, 8 vowels, some numbers, and the first 5 numbers of your private bitcoin key",
+    "If you send your password to 15 friends before 143 minutes and then step three times on the same trackpad, you will get the name of the person who loves you.",
 ];
 
 ?>
